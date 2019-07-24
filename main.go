a83cd383bddb236712c6e799851911685ed7ad3f
rHermes <teodor_spaeren@riseup.net>
2019-07-24 06:34:49 +0200

    Add license notice

--- main.go
+++ main.go
@@ -1,3 +1,22 @@
+// Copyright (c) 2019 Teodor Spæren
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 package main
 
 import (
@@ -55,8 +74,8 @@ func performJobs(s *store.Store) error {
 				log.Printf("We have been ratelimited, waiting 10 minutes\n")
 				time.Sleep(10 * time.Minute)
 			} else {
-				log.Printf("There was some other error, waiting 1 minute\n")
-				time.Sleep(1 * time.Minute)
+				log.Printf("There was some other error, waiting 10 seconds\n")
+				time.Sleep(10 * time.Second)
 			}
 		}
 		time.Sleep(1 * time.Second)
